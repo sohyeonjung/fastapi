28f3d80e502df8c4d663d4229579c07d589a5822
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-11-17 18:55:35 +0900

    feat: complete fetch

--- server/app/api.py
+++ server/app/api.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from todo import todo_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def welcome() -> dict:
-    return {"message": "Sohyeon Jung TODO Server"}
+    return {"message": "Sohyeon Jung TODO"}
 
 app.include_router(todo_router)
